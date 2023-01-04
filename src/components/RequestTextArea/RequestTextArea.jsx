aafbf5bc90e421832e68c67a8b1dcb6a398c7177
dmitridrozdov <dm.drozdov@gmail.com>
2023-01-05 10:54:37 +1100

    Update RequestTextArea.jsx

--- src/components/RequestTextArea/RequestTextArea.jsx
+++ src/components/RequestTextArea/RequestTextArea.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
+import { Button } from '@material-ui/core'
 
-class RequestTextArea extends React.Component {
-  render() {
-    return (
-      <textarea
-        value={this.props.value}
-        onChange={this.props.onChange}
-      />
-    )
-  }
-}
+const RequestTextArea = ({ value, onChange, onSave }) => (
+  <div>
+    <textarea
+      value={value}
+      onChange={onChange}
+      style={{ width: '100%', height: '300px' }}
+    />
+    <Button variant="contained" onClick={onSave}>
+      Save
+    </Button>
+  </div>
+);
 
 export default RequestTextArea
