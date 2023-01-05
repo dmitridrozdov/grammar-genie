55e62e7084f757caacaa6aa18a90b273173b79dc
dmitridrozdov <dm.drozdov@gmail.com>
2023-01-06 10:51:54 +1100

    Create Intro.jsx

---
+++ src/components/Intro/Intro.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Intro = () => {
+  return (
+    <div>Grammatically correct sentences are important for a number of reasons:
+
+    Clarity: Grammatically correct sentences are easier to understand because they follow the rules of syntax and structure, making it clear what the writer is trying to say.
+    
+    Professionalism: Writing grammatically correct sentences is important in professional settings, as it shows that you have a strong command of the language and are able to communicate effectively.
+    
+    Credibility: Grammatically correct sentences increase the credibility of the writer, as they demonstrate attention to detail and a level of professionalism.
+    
+    Audience: Different audiences may have different expectations for the level of grammar used in writing. For example, academic writing is expected to be more formal and grammatically correct, while casual social media posts may be more relaxed.
+    
+    Personal expression: Correct grammar allows you to effectively express yourself and your ideas, and helps you to be understood by your audience.
+    
+    Overall, writing grammatically correct sentences is important for effective communication, professionalism, and credibility.
+    </div>
+  )
+}
+
+export default Intro
