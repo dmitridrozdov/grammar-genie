2029ab833a2c7e28ace2a784b3699658e1db8f2d
dmitridrozdov <dm.drozdov@gmail.com>
2023-01-05 15:46:44 +1100

    Update RequestTextArea.jsx

--- src/components/RequestTextArea/RequestTextArea.jsx
+++ src/components/RequestTextArea/RequestTextArea.jsx
@@ -1,17 +1,51 @@
-import React from 'react'
-import { Button } from '@material-ui/core'
+import React from 'react';
+import { Button, TextField } from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles';
 
-const RequestTextArea = ({ value, onChange, onSave }) => (
-  <div>
-    <textarea
+const styles = {
+  container: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    margin: '10px',
+    border: '1px solid #ccc',
+  },
+  textArea: {
+    width: '400px',
+    height: '40px',
+    fontSize: '16px',
+    lineHeight: '24px',
+    fontFamily: 'Roboto',
+    borderRadius: '4px',
+    
+    padding: '16px',
+    '&:focus': {
+      outline: 'none',
+      borderColor: '#555',
+    },
+  },
+  button: {
+    width: '80px',
+    height: '40px',
+  },
+};
+
+const RequestTextArea = ({ value, onChange, onSend, classes }) => (
+  <div className={classes.container}>
+    <TextField
       value={value}
       onChange={onChange}
-      style={{ width: '100%', height: '300px' }}
+      className={classes.textArea}
+      InputProps={{
+        disableUnderline: true,
+      }}
     />
-    <Button variant="contained" onClick={onSave}>
-      Save
+    <Button variant="contained" onClick={onSend} className={classes.button}>
+      Send
     </Button>
   </div>
 );
 
-export default RequestTextArea
+export default withStyles(styles)(RequestTextArea);
+
