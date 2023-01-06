7b09cd209c18f61d4d80f6e9108ac5098014a5fb
dmitridrozdov <dm.drozdov@gmail.com>
2023-01-06 19:09:34 +1100

    Create AppBar.jsx

---
+++ src/components/AppBar/AppBar.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import useStyles from './styles'
+import { Typography, Toolbar, Grid } from '@material-ui/core'
+
+const AppBar = () => {
+  const classes = useStyles()
+  return (
+
+      <Grid container spacing={2} xs={12} >
+        <Grid item container xs={8}>
+            <Toolbar>
+              <img alt='Grammar Genie' src='/src/assets/logo.png' width='50' height='50' className={classes.logo}/>
+              <Typography className={classes.headerText}>Grammar Genie</Typography>
+            </Toolbar>
+          </Grid>
+        <Grid xs={4} container >
+          
+        </Grid>
+      </Grid>
+
+
+      
+  )
+}
+
+export default AppBar
