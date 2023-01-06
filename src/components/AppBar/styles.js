ff9083bffd50166971ff76a3f344b3d465e63e40
dmitridrozdov <dm.drozdov@gmail.com>
2023-01-06 19:09:36 +1100

    Create styles.js

---
+++ src/components/AppBar/styles.js
@@ -0,0 +1,23 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles((theme) => ({
+
+    logo: {
+        margin: '20px',
+    },
+
+    headerText: {
+        fontFamily: 'Fredoka One',
+        marginLeft: '10px',
+        fontSize: '24px',
+        color: '#326c99'
+    },
+
+    leftPart: {
+        background: 'blue',
+    },
+    rightPart: {
+        background: 'red',
+    },
+
+}));
