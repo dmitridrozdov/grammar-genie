8e5e915f922f6c0a3084fb69bea510b2333ecd57
dmitridrozdov <dm.drozdov@gmail.com>
2023-01-07 12:51:48 +1100

    Create styles.js

---
+++ src/components/Intro/styles.js
@@ -0,0 +1,20 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles((theme) => ({
+
+    headerText: {
+        fontFamily: 'Montserrat',
+        fontSize: '14px',
+        color: '#326c99',
+        fontWeight: 'bold',
+    },
+
+    text: {
+        fontFamily: 'Kanit',
+        fontSize: '14px',
+        color: '#326c99',
+    },
+
+
+
+}));
