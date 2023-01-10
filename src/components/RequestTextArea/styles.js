17cded22cd31f1900a8b3e910b8f2dea013691d6
dmitridrozdov <dm.drozdov@gmail.com>
2023-01-10 15:57:10 +1100

    Update styles.js

---
+++ src/components/RequestTextArea/styles.js
@@ -0,0 +1,32 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles((theme) => ({
+
+    container: {
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        margin: '10px',
+        border: '1px solid #326c99',
+      },
+      textArea: {
+        width: '100%',
+        height: '20px',
+        fontSize: '16px',
+        lineHeight: '24px',
+        fontFamily: 'Roboto',
+        borderRadius: '4px',
+        
+        padding: '16px',
+        '&:focus': {
+          outline: 'none',
+          borderColor: '#326c99',
+        },
+      },
+      button: {
+        width: '80px',
+        height: '40px',
+      },
+
+}));
