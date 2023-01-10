1012a0e4d7d0b8e14ddef4383ff3f67f796d811a
dmitridrozdov <dm.drozdov@gmail.com>
2023-01-10 16:00:35 +1100

    Update AppBar.jsx

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -7,15 +7,15 @@ const AppBar = () => {
   return (
 
       <Grid container spacing={2} xs={12} >
-        <Grid item container xs={4} className={classes.rightPart}>
+        <Grid item container xs={12} >
             <Toolbar>
               <img alt='Grammar Genie' src='/src/assets/logo.png' width='50' height='50' className={classes.logo}/>
               <Typography className={classes.headerText}>Grammar Genie</Typography>
             </Toolbar>
           </Grid>
-        <Grid xs={8} container className={classes.leftPart}>
+        {/* <Grid xs={8} container className={classes.leftPart}>
           
-        </Grid>
+        </Grid> */}
       </Grid>
 
 
