ababaa6a0b11293ba44edc3382d71ea0ff1b6d10
dmitridrozdov <dm.drozdov@gmail.com>
2023-01-11 11:21:56 +1100

    Update AppBar.jsx

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -5,21 +5,12 @@ import { Typography, Toolbar, Grid } from '@material-ui/core'
 const AppBar = () => {
   const classes = useStyles()
   return (
-
-      <Grid container spacing={2} xs={12} >
-        <Grid item container xs={12} >
-            <Toolbar>
-              <img alt='Grammar Genie' src='/src/assets/logo.png' width='50' height='50' className={classes.logo}/>
-              <Typography className={classes.headerText}>Grammar Genie</Typography>
-            </Toolbar>
-          </Grid>
-        {/* <Grid xs={8} container className={classes.leftPart}>
-          
-        </Grid> */}
-      </Grid>
-
-
-      
+    <Grid item container xs={12}>
+        <Toolbar>
+          <img alt='Grammar Genie' src='/src/assets/logo.png' width='50' height='50' className={classes.logo}/>
+          <Typography className={classes.headerText}>Grammar Genie</Typography>
+        </Toolbar>
+    </Grid>      
   )
 }
 
