c23565ec78f4bdf7d222f7e4ca6feee086a0f3e2
dmitridrozdov <dm.drozdov@gmail.com>
2023-01-11 14:01:33 +1100

    Update CorrectResponse.jsx

--- src/components/CorrectResponse/CorrectResponse.jsx
+++ src/components/CorrectResponse/CorrectResponse.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
+import useStyles from './styles'
+import { Button } from '@material-ui/core'
 
-const CorrectResponse = () => {
+
+const CorrectResponse = ({ text }) => {
+  const classes = useStyles()
+  const [copySuccess, setCopySuccess] = useState('Copy')
+
+  const copyToClipBoard = async textToCopy => {
+    try {
+      await navigator.clipboard.writeText(textToCopy);
+      setCopySuccess('Copied!');
+      setTimeout(() => {setCopySuccess('Copy')}, 3000); // Reset after 3 seconds
+    } catch (err) {
+      setCopySuccess('Failed to copy!');
+    }
+  }
+
   return (
-    <div>CorrectResponse</div>
+    <div className={classes.box}>
+      <div className={classes.copyButtonDiv}>
+        <Button className={classes.copyButton} onClick={() => copyToClipBoard(text)}>{copySuccess}</Button>
+      </div>
+      <div className={classes.text}>{text}</div>
+    </div>
   )
 }
 
