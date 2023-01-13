1f7c8c4ede674103d470df490731e3dbf71c092a
dmitridrozdov <dm.drozdov@gmail.com>
2023-01-13 13:49:53 +1100

    Update styles.js

--- src/components/AppBar/styles.js
+++ src/components/AppBar/styles.js
@@ -7,7 +7,7 @@ export default makeStyles((theme) => ({
     },
 
     headerText: {
-        fontFamily: 'Fredoka One',
+        fontFamily: 'Montserrat',
         marginLeft: '10px',
         fontSize: '24px',
         color: '#326c99'
