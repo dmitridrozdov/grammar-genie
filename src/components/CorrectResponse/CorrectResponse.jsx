4cba3b8bbe0b58a4573ead241fefaf3b4b8ada43
dmitridrozdov <dm.drozdov@gmail.com>
2023-01-16 05:51:42 +1000

    Update CorrectResponse.jsx

--- src/components/CorrectResponse/CorrectResponse.jsx
+++ src/components/CorrectResponse/CorrectResponse.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import useStyles from './styles'
-import { Button, IconButton } from '@material-ui/core'
+import { Fade, IconButton } from '@material-ui/core'
 import ContentCopyIcon from '@mui/icons-material/ContentCopy'
 import DoneIcon from '@mui/icons-material/Done'
 
@@ -20,14 +20,18 @@ const CorrectResponse = ({ text }) => {
   }
 
   return (
-    <div className={classes.box}>
-      <div className={classes.copyButtonDiv}>
-        <IconButton onClick={() => copyToClipBoard(text)} className={classes.copyButton}>
-          {copySuccess}
-        </IconButton>
+    <Fade in={true}>
+      <div className={classes.box}>
+        
+          <div className={classes.copyButtonDiv}>
+            <IconButton onClick={() => copyToClipBoard(text)} className={classes.copyButton}>
+              {copySuccess}
+            </IconButton>
+          </div>
+          <div className={classes.text}>{text}</div>
+        
       </div>
-      <div className={classes.text}>{text}</div>
-    </div>
+    </Fade>
   )
 }
 
