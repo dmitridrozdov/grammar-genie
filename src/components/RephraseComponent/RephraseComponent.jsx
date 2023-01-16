75fb9d017c52cbcf4457c8147782aa700ad5e341
dmitridrozdov <dm.drozdov@gmail.com>
2023-01-16 18:05:29 +1000

    Update RephraseComponent.jsx

--- src/components/RephraseComponent/RephraseComponent.jsx
+++ src/components/RephraseComponent/RephraseComponent.jsx
@@ -2,21 +2,25 @@ import React from 'react'
 import CorrectResponse from '../CorrectResponse/CorrectResponse';
 import { Grid } from '@material-ui/core';
 
-const RephraseComponent = ({ text }) => {
+const RephraseComponent = ({ response }) => {
 
     const parseString = str => {
         return str.split(/1\.|2\.|3\./).map(item => item.trim()).filter(item => item !== '')
     }
     
-    console.log(text)
-    const array = parseString(text)
+    console.log(response)
+    const array = parseString(response)
     console.log(array)
+
+    const generateUniqueKey = () => {
+        return Math.random().toString(36).substring(2, 5) + Math.random().toString(36).substring(2, 5)
+    }
 
     return (
         <>
             {array.map(rephrase => (
-                <Grid item xs={12}> 
-                    <CorrectResponse key={rephrase} text={rephrase} />
+                <Grid item xs={12} key={generateUniqueKey()}> 
+                    <CorrectResponse response={rephrase} />
                 </Grid>
             ))}
         </>
